af7d2a5a2c18d9a04ae8977cd4ab45f231317158
Leandro Abreu <leandro.sabreu@uvvnet.com.br>
2022-08-23 13:16:26 -0300

    02 - Componentes e Estilos

--- orgs-cesta/App.js
+++ orgs-cesta/App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, SafeAreaView, View } from 'react-native';
+import { useFonts, 
+  Montserrat_400Regular, 
+  Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+
+import Cesta from './src/telas/Cesta';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Alura!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold
+  });
+
+if (!fontCarregada){
+  return <View/>
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <Cesta />
+    </SafeAreaView>
+  );
+}
