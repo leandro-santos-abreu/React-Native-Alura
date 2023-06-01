eabe5a74f47c9803cce24150579e0f508b2481e7
Leandro Abreu <leandro.sabreu@uvvnet.com.br>
2023-06-01 17:49:58 -0300

    #Aula 04. Testes de Hook e Componente

---
+++ react-native-criando-testes-para-sua-aplicacao-main/__tests__/hooks/useListaLeiloes.test.js
@@ -0,0 +1,48 @@
+import useListaLeiloes from "../../src/hooks/useListaLeiloes"
+import { renderHook, act } from "@testing-library/react-hooks"
+
+import { obtemLeiloes } from "../../src/repositorio/leilao";
+
+jest.mock('../../src/repositorio/leilao');
+
+const mockLeiloes = [
+    {
+        id: 1,
+        nome: 'Leilão',
+        descricao: 'Descrição do leilão'
+    }
+]
+
+const mockLeiloesAtualizada = [
+    {
+        id: 1,
+        nome: 'Leilão',
+        descricao: 'Descrição do leilão'
+    },
+    {
+        id: 2,
+        nome: 'Leilão 2',
+        descricao: 'Descrição do leilão 2'
+    }
+]
+
+
+describe('hooks/useListaLeiloes', () => {
+
+    it('deve retornar uma lista de leiloes e uma função para atualizar', async () => {
+
+        obtemLeiloes.mockImplementation(() => mockLeiloes)
+
+        const { result, waitForNextUpdate } = renderHook(() => useListaLeiloes());
+        expect(result.current[0]).toEqual([]);
+
+        await waitForNextUpdate({timeout: false});
+        expect(result.current[0]).toEqual(mockLeiloes);
+
+        obtemLeiloes.mockImplementation(() => mockLeiloesAtualizada)
+
+        await act(() => result.current[1]());
+        expect(result.current[0]).toEqual(mockLeiloesAtualizada);
+        
+    });
+})
